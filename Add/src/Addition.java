58f2d902376ca20fbb9ba8052ae12c823483d775
ZehraShan <ZehraShan@192.168.100.6>
2020-03-13 15:20:14 +0530

    first commit

---
+++ Add/src/Addition.java
@@ -0,0 +1,13 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=45;
+		int c=a+b;
+		System.out.println("printing="+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
